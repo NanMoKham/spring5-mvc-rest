f1e856f565b2ff5d0ce928e96edeb084f0487bef
nanmokham <nanmokham@isgm2.com>
2020-12-21 13:30:25 +0630

    add: mapStruct intelliJ plugin
    Signed-off-by: nanmokham <nanmokham@isgm2.com>

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -2,11 +2,17 @@ package guru.springfamework.api.v1.model;
 
 import lombok.Data;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by jt on 9/24/17.
  */
 @Data
 public class CategoryDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
